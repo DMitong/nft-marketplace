a42a390cf6a657db4cbe213958aefb7d59619f8e
DMitong <mitongdapal@gmail.com>
2023-10-04 06:58:19 +0100

    more

--- test/Marketplace.t.sol
+++ test/Marketplace.t.sol
@@ -2,45 +2,58 @@
 pragma solidity 0.8.19;
 
 import "forge-std/Test.sol";
-import "../src/Marketplace.sol"; 
-import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
+import "../src/Marketplace.sol";
 
-contract MarketplaceTest is Test, ERC721Holder {
+interface IMockNft is IERC721 {
+    function safeMint(address to, uint256 tokenId) external;
+ }
+ 
+contract ERC721MarketplaceTest is Test {
+  using SignatureChecker for address;
 
-  ERC721Marketplace marketplace;
+  ERC721Marketplace public marketplace;
+  uint256 internal creatorPriv;
+  uint256 internal spenderPriv;
+  address creator;
+  address tokenOwner;
+  address tokenAddress;
+  uint256 tokenID;
+  uint256 price;
+  uint256 deadline;
+  bool executed;
+  bool isActive;
+  bytes signature;
 
-  address creator = vm.addr(1);
-  address buyer = vm.addr(2);
-
-  uint256 tokenId = 1;
-  address nftContract = 0x0000000000000000000000000000000000000001;
-
-  function setUp() public {
-    marketplace = new ERC721Marketplace();
+   function genHash(address _tokenAddress, uint256 _tokenID, uint256 _price, uint256 _deadline) public pure returns (bytes32) {
+    return keccak256(abi.encodePacked(tokenAddress, tokenID, price, deadline));
   }
 
-  function testCreateAndFulfillOrder() public {
+   function setUp() public {
+         marketplace = new ERC721Marketplace();
+
+         creatorPriv = 67890;
+         creator = vm.addr(creatorPriv);
+         tokenAddress = 0xAc4D78798804e2463E7785698d51239CfA768DAd;
+         tokenID = 2972;
+         price = 2 ether;
+         deadline = 2 days;
+
+         IMockNft(tokenAddress).safeMint(creator, tokenID);
     vm.startPrank(creator);
-    bytes32 orderHash = marketplace.createOrder(nftContract, tokenId, 1 ether, block.timestamp + 1 days);
-    vm.stopPrank();
+    bytes32 hashMsg = genHash(tokenAddress, tokenID, price, deadline);
+    signature = msg.sender.SignatureChecker(hashMsg);
+  }
 
-    bytes memory signature = signOrder(orderHash);
+  function testFulfillOrder() public {
+    // Create an order to sell the ERC721 token
+    bytes32 orderHash = keccak256(abi.encodePacked(tokenAddress, tokenID, price, deadline, creator));
 
-    vm.prank(buyer);
+    // Fulfill the order
     marketplace.fulfillOrder(orderHash, signature);
 
-    assertEq(IERC721(nftContract).ownerOf(tokenId), buyer);
-
-    assertTrue(emittedOrderFulfilled(orderHash));
+    // Check that the ERC721 token has been transferred to the buyer
+    require(IMockNft.ownerOf(tokenID) == address(this), "ERC721 token not transferred to buyer");
   }
-
-  function signOrder(bytes32 orderHash) public view returns (bytes memory) {
-    return vm.sign(creator, orderHash); 
-  }
-
-  function emittedOrderFulfilled(bytes32 orderHash) public view returns (bool) {
-    return logExists(ERC721Marketplace.OrderFulfilled(orderHash, buyer, creator, nftContract, tokenId, 1 ether)); 
-  }
-
 }
