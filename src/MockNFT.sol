4619fb07347d4e17659b1db6c8c684503bc96a3d
DMitong <mitongdapal@gmail.com>
2023-10-06 16:25:39 +0100

    made updates

--- src/MockNFT.sol
+++ src/MockNFT.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity 0.8.19;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract TestNFT is ERC721("TestNFT", "TFT") {
     function tokenURI(
-        uint256 id
+        uint256 tokenID
     ) public view virtual override returns (string memory) {
         return "Scam";
     }
--- test/Marketplace.t.sol
+++ test/Marketplace.t.sol
@@ -22,7 +22,7 @@ contract Marketplace is
     uint256 privKey1;
     uint256 privKey2;
 
-    ERC721Marketplace.Order orderDetails;
+    ERC721ERC721Marketplace.Order orderDetails;
 
     function setUp() public {
         marketplace = new ERC721Marketplace();
@@ -32,7 +32,7 @@ contract Marketplace is
         (user1, privKey1) = mkaddr("user1");
         (user2, privKey2) = mkaddr("user2");
 
-        orderDetails = ERC721Marketplace.Order({
+        orderDetails = ERC721ERC721Marketplace.Order({
             tokenAddress: address(erc721),
             tokenID: 1,
             price: 1 ether,
@@ -47,4 +47,215 @@ contract Marketplace is
     }
 
     // Tests
+    function testOwnerCannotCreateOrder() public {
+        orderDetails.creator = user2;
+        switchSigner(user2);
+
+        vm.expectRevert(ERC721ERC721Marketplace.NotOwner.selector);
+        marketplace.createOrder(orderDetails);
+    }
+
+    function testNonApprovedNFT() public {
+        switchSigner(user1);
+        vm.expectRevert(ERC721ERC721Marketplace.NotApproved.selector);
+        marketplace.createOrder(orderDetails);
+    }
+
+    function testMinPriceTooLow() public {
+        switchSigner(user1);
+        erc721.setApprovalForAll(address(marketplace), true);
+        orderDetails.price = 0;
+        vm.expectRevert(ERC721Marketplace.MinPriceTooLow.selector);
+        marketplace.createOrder(orderDetails);
+    }
+
+    function testMinDeadline() public {
+        switchSigner(user1);
+        erc721.setApprovalForAll(address(marketplace), true);
+        vm.expectRevert(ERC721Marketplace.DeadlineTooSoon.selector);
+        marketplace.createOrder(orderDetails);
+    }
+
+    function testMinDuration() public {
+        switchSigner(user1);
+        erc721.setApprovalForAll(address(marketplace), true);
+        orderDetails.deadline = uint88(block.timestamp + 59 minutes);
+        vm.expectRevert(ERC721Marketplace.MinDurationNotMet.selector);
+        marketplace.createOrder(orderDetails);
+    }
+
+    function testValidSig() public {
+        switchSigner(user1);
+        erc721.setApprovalForAll(address(marketplace), true);
+        orderDetails.deadline = uint88(block.timestamp + 120 minutes);
+        orderDetails.sig = constructSig(
+            orderDetails.tokenAddress,
+            orderDetails.tokenID,
+            orderDetails.price,
+            orderDetails.deadline,
+            orderDetails.creator,
+            privKey2
+        );
+        vm.expectRevert(ERC721Marketplace.InvalidSignature.selector);
+        marketplace.createOrder(orderDetails);
+    }
+
+    // EDIT ORDER
+    function testEditNonValidOrder() public {
+        switchSigner(user1);
+        vm.expectRevert(ERC721Marketplace.OrderNotExistent.selector);
+        marketplace.editOrder(1, 0, false);
+    }
+
+    function testEditOrderNotOwner() public {
+        switchSigner(user1);
+        erc721.setApprovalForAll(address(marketplace), true);
+        orderDetails.deadline = uint88(block.timestamp + 120 minutes);
+        orderDetails.sig = constructSig(
+            orderDetails.tokenAddress,
+            orderDetails.tokenAddressID,
+            orderDetails.price,
+            orderDetails.deadline,
+            orderDetails.creator,
+            privKey1
+        );
+        // vm.expectRevert(ERC721Marketplace.OrderNotExistent.selector);
+        uint256 orderId = marketplace.createOrder(orderDetails);
+
+        switchSigner(user2);
+        vm.expectRevert(ERC721Marketplace.NotOwner.selector);
+        marketplace.editOrder(orderId, 0, false);
+    }
+
+    function testEditOrder() public {
+        switchSigner(user1);
+        erc721.setApprovalForAll(address(marketplace), true);
+        orderDetails.deadline = uint88(block.timestamp + 120 minutes);
+        orderDetails.sig = constructSig(
+            orderDetails.tokenAddress,
+            orderDetails.tokenAddressID,
+            orderDetails.price,
+            orderDetails.deadline,
+            orderDetails.creator,
+            privKey1
+        );
+        uint256 orderId = marketplace.createOrder(orderDetails);
+        marketplace.editOrder(orderId, 0.01 ether, false);
+
+        ERC721ERC721Marketplace.Order memory t = marketplace.getOrder(orderId);
+        assertEq(t.price, 0.01 ether);
+        assertEq(t.active, false);
+    }
+
+    // EXECUTE ORDER
+    function testFulfillNonValidOrder() public {
+        switchSigner(user1);
+        vm.expectRevert(ERC721Marketplace.OrderNotExistent.selector);
+        marketplace.fulfillOrder(1);
+    }
+
+    function testFulfillExpiredOrder() public {
+        switchSigner(user1);
+        erc721.setApprovalForAll(address(marketplace), true);
+    }
+
+    function testFulfillOrderNotActive() public {
+        switchSigner(user1);
+        erc721.setApprovalForAll(address(marketplace), true);
+        orderDetails.deadline = uint88(block.timestamp + 120 minutes);
+        orderDetails.sig = constructSig(
+            orderDetails.tokenAddress,
+            orderDetails.tokenAddressID,
+            orderDetails.price,
+            orderDetails.deadline,
+            orderDetails.creator,
+            privKey1
+        );
+        uint256 orderId = marketplace.createOrder(orderDetails);
+        marketplace.editOrder(orderId, 0.01 ether, false);
+        switchSigner(user2);
+        vm.expectRevert(ERC721Marketplace.OrderNotActive.selector);
+        marketplace.fulfillOrder(orderId);
+    }
+
+    function testFulfillPriceNotMet() public {
+        switchSigner(user1);
+        erc721.setApprovalForAll(address(marketplace), true);
+        orderDetails.deadline = uint88(block.timestamp + 120 minutes);
+        orderDetails.sig = constructSig(
+            orderDetails.tokenAddress,
+            orderDetails.tokenAddressID,
+            orderDetails.price,
+            orderDetails.deadline,
+            orderDetails.creator,
+            privKey1
+        );
+        uint256 orderId = marketplace.createOrder(orderDetails);
+        switchSigner(user2);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                ERC721Marketplace.PriceNotMet.selector,
+                orderDetails.price - 0.9 ether
+            )
+        );
+        marketplace.fulfillOrder{value: 0.9 ether}(orderId);
+    }
+
+    function testFulfillPriceMismatch() public {
+        switchSigner(user1);
+        erc721.setApprovalForAll(address(marketplace), true);
+        orderDetails.deadline = uint88(block.timestamp + 120 minutes);
+        orderDetails.sig = constructSig(
+            orderDetails.tokenAddress,
+            orderDetails.tokenAddressID,
+            orderDetails.price,
+            orderDetails.deadline,
+            orderDetails.creator,
+            privKey1
+        );
+        uint256 orderId = marketplace.createOrder(orderDetails);
+        switchSigner(user2);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                ERC721Marketplace.PriceMismatch.selector,
+                orderDetails.price
+            )
+        );
+        marketplace.fulfillOrder{value: 1.1 ether}(orderId);
+    }
+
+    function testFulfill() public {
+        switchSigner(user1);
+        erc721.setApprovalForAll(address(marketplace), true);
+        orderDetails.deadline = uint88(block.timestamp + 120 minutes);
+        // orderDetails.price = 1 ether;
+        orderDetails.sig = constructSig(
+            orderDetails.tokenAddress,
+            orderDetails.tokenAddressID,
+            orderDetails.price,
+            orderDetails.deadline,
+            orderDetails.creator,
+            privKey1
+        );
+        uint256 orderId = marketplace.createOrder(orderDetails);
+        switchSigner(user2);
+        uint256 user1BalanceBefore = user1.balance;
+
+        marketplace.fulfillOrder{value: orderDetails.price}(orderId);
+
+        uint256 user1BalanceAfter = user1.balance;
+
+        ERC721ERC721Marketplace.Order memory t = marketplace.getOrder(orderId);
+        assertEq(t.price, 1 ether);
+        assertEq(t.active, false);
+
+        assertEq(t.active, false);
+        assertEq(
+            ERC721(orderDetails.tokenAddress).ownerOf(
+                orderDetails.tokenAddressID
+            ),
+            user2
+        );
+        assertEq(user1BalanceAfter, user1BalanceBefore + orderDetails.price);
+    }
 }
